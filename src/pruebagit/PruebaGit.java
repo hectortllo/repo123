5bfa9bf06f4195e67faad219e736fe8cf1d4b4e9
hectortllo <hectortllo@192.168.1.8>
2018-01-23 17:21:52 -0600

    Mi aporte fue la encriptación de un texto en PruebaGit.java

--- src/pruebagit/PruebaGit.java
+++ src/pruebagit/PruebaGit.java
@@ -5,6 +5,8 @@
  */
 package pruebagit;
 
+import org.apache.commons.codec.digest.DigestUtils;
+
 /**
  *
  * @author hectortllo
@@ -18,6 +20,10 @@ public class PruebaGit {
         System.out.println("Hola mundo");
         System.out.println("hola mundo (en minúsculas)");
         System.out.println("hola mundo (desde github.com)");
+        
+        String textoSinEncriptar="sergio"; 
+        String textoEncriptadoConMD5=DigestUtils.md5Hex(textoSinEncriptar); 
+        System.out.println("Texto Encriptado con MD5 : "+textoEncriptadoConMD5);
     }
     
 }
